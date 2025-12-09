aafb5d0e2b70755b3d26d3bc92c4a095f45e6925
xerrial <dimass1129@gmail.com>
2025-12-10 03:25:16 +1000

    MVP state achieved

--- src/drwlm.c
+++ src/drwlm.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <assert.h>
 
+#include <sys/uio.h>
+
 #include <pthread.h>
 
 #include <corosync/cpg.h>
@@ -20,6 +22,19 @@ typedef enum
     WR,
     RWLOCK_MODES_NUM
 } rwlock_mode_t;
+
+char *rwlock_mode_to_str(rwlock_mode_t mode)
+{
+    switch (mode)
+    {
+    case NL: return "NL";
+    case RD: return "RD";
+    case IW: return "IW";
+    case WR: return "WR";
+    default:
+        return "ERROR";
+    }
+}
 
 bool modes_compatible(rwlock_mode_t current, rwlock_mode_t desired)
 {
@@ -40,17 +55,43 @@ bool modes_compatible(rwlock_mode_t current, rwlock_mode_t desired)
     return compatibility_table[current][desired];
 }
 
+int send_msg(cpg_handle_t handle, void *msg, size_t len)
+{
+    cs_error_t csrv = CS_OK;
+
+    struct iovec vec = { msg, len };
+    int vec_len = 1;
+
+    if ((csrv = cpg_mcast_joined(handle, CPG_TYPE_FIFO, &vec, vec_len)) != CS_OK)
+    {
+        fprintf(stderr, "Failed to mcast joined: %s (%s)\n", cs_strerror(csrv), strerror(errno));
+    }
+
+    return csrv;
+}
+
 typedef unsigned char node_id_t;
 typedef unsigned int  xa_id_t;
 
+static int g_own_id = -1;
+static int g_nodes_count = -1;
+
 node_id_t own_id()
 {
-    static_assert(0, "Not implemented");
+    assert(g_own_id != -1);
+    return g_own_id;
 }
 
 size_t nodes_count()
 {
-    static_assert(0, "Not implemented");
+    assert(g_nodes_count != -1);
+    return g_nodes_count;
+}
+
+xa_id_t next_xid()
+{
+    static xa_id_t xid = 0;
+    return xid++;
 }
 
 typedef enum {
@@ -66,6 +107,17 @@ typedef enum {
     GRANTED = 0,
     DENIED
 } reply_t;
+
+char *reply_to_str(reply_t reply)
+{
+    switch (reply)
+    {
+    case GRANTED: return "GRANTED";
+    case DENIED:  return "DENIED";
+    default:
+        return "ERROR";
+    }
+}
 
 typedef struct
 {
@@ -101,8 +153,13 @@ static lock_t lock = {
     .granted_count = 0
 };
 
-void on_request(request_t *req)
+void on_request(cpg_handle_t handle, request_t *req)
 {
+    printf("REQUEST(from=%u, xid=%u, mode=%s)\n",
+            req->from,
+            req->xid,
+            rwlock_mode_to_str(req->mode));
+
     if (req->from == own_id())
     {
         lock.transitioning = true;
@@ -116,28 +173,27 @@ void on_request(request_t *req)
             .header.type = RESPONSE,
             .to = req->from,
             .from = own_id(),
-            .xid = req->xid
+            .xid = req->xid,
+            .reply = modes_compatible(lock.mode, req->mode) ? GRANTED : DENIED
         };
 
-        if (modes_compatible(lock.mode, req->mode))
-        {
-            resp.reply = GRANTED;
-            // send granted response
-        }
-        else
-        {
-            resp.reply = DENIED;
-            // send denied response
-        }
+        if (send_msg(handle, &resp, sizeof(response_t)) != CS_OK)
+            fprintf(stderr, "Failed to send response\n");
     }
 
     return;
 }
 
-void on_response(response_t *resp)
+void on_response(cpg_handle_t handle, response_t *resp)
 {
     if (resp->to != own_id())
         return;
+
+    printf("RESPONSE(to=%u, from=%u, xid=%u, reply=%s)\n",
+            resp->to,
+            resp->from,
+            resp->xid,
+            reply_to_str(resp->reply));
 
     if (resp->xid != lock.pending_xid)
         return;
@@ -185,22 +241,22 @@ void deliver_cb(cpg_handle_t      handle,
                 void             *msg,
                 size_t            msg_len)
 {
-    printf("deliver_cb(): message (len=%zu) "
-           "group '%.*s' "
-           "from %" PRIu32 "/%" PRIu32 " "
-           ": '%.*s'\n",
-           msg_len,
-           groupName->length, groupName->value,
-           nodeid, pid,
-           msg_len, (const char *)msg);
+    // printf("deliver_cb(): message (len=%zu) "
+    //        "group '%.*s' "
+    //        "from %" PRIu32 "/%" PRIu32 " "
+    //        ": '%.*s'\n",
+    //        msg_len,
+    //        groupName->length, groupName->value,
+    //        nodeid, pid,
+    //        msg_len, (const char *)msg);
 
     switch (((message_header_t *)msg)->type)
     {
     case REQUEST:
-        on_request(msg);
+        on_request(handle, msg);
         break;
     case RESPONSE:
-        on_response(msg);
+        on_response(handle, msg);
         break;
     default:
         fprintf(stderr, "Unexpected message type '%d'", ((message_header_t *)msg)->type);
@@ -217,7 +273,8 @@ static void confchg_cb(cpg_handle_t         handle,
                        const cpg_address_t *joined_list,
                        size_t               joined_list_entries)
 {
-    printf("confchg_cb()\n");
+    printf("confchg_cb(member_list_entries=%zu)\n", member_list_entries);
+    g_nodes_count = member_list_entries;
 }
 
 static void totem_confchg_cb(cpg_handle_t    handle,
@@ -250,17 +307,53 @@ int main(void)
         return EXIT_FAILURE;
     }
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, dispatch_thread, &handle);
-
     cpg_name_t cpg_name;
     cpg_name.length = strlen(DRWLM_CPG_NAME);
     strncpy(cpg_name.value, DRWLM_CPG_NAME, cpg_name.length);
 
     if ((csrv = cpg_join(handle, &cpg_name)) != CS_OK)
     {
-        fprintf(stderr, "Failed to join cpg\n");
+        fprintf(stderr, "Failed to join cpg: %s (%s)\n", cs_strerror(csrv), strerror(errno));
         goto exit;
+    }
+
+    if ((csrv = cpg_local_get(handle, &g_own_id)) != CS_OK)
+    {
+        fprintf(stderr, "Failed to get local id: %s (%s)\n", cs_strerror(csrv), strerror(errno));
+        goto exit;
+    }
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, dispatch_thread, &handle);
+
+    char buffer[256];
+    while (fgets(buffer, 256, stdin) != NULL)
+    {
+        rwlock_mode_t mode = NL;
+
+        if (!strncmp(buffer, "NL", 2))
+            mode = NL;
+        else if (!strncmp(buffer, "RD", 2))
+            mode = RD;
+        else if (!strncmp(buffer, "IW", 2))
+            mode = IW;
+        else if (!strncmp(buffer, "WR", 2))
+            mode = WR;
+        else
+        {
+            fprintf(stderr, "Wrong input");
+            continue;
+        }
+
+        request_t req = {
+            .header.type = REQUEST,
+            .from = own_id(),
+            .mode = mode,
+            .xid = next_xid()
+        };
+
+        if (send_msg(handle, &req, sizeof(request_t)) != CS_OK)
+            fprintf(stderr, "Failed to send request\n");
     }
 
     pthread_join(thread, NULL);
